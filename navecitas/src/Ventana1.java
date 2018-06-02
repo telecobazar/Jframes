c97ae24784b9e6ade03c38f275ff17a845528b32
telecobazar <32357607+telecobazar@users.noreply.github.com>
2018-06-02 14:19:13 +0200

    actualizada ventana1

--- navecitas/src/Ventana1.java
+++ navecitas/src/Ventana1.java
@@ -1,10 +1,16 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Dimension;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JTextField;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+
 
 /**
  * Clase Ventana
@@ -18,9 +24,8 @@ public class Ventana1 extends JFrame implements ActionListener {
 	 * 
 	 */
 	private static final long serialVersionUID = 2813843257427348561L;
-	private JLabel texto1, texto2, texto3;           // etiqueta o texto no editable
-    private JTextField caja1, caja2, caja3;        // caja de texto, para insertar datos
-    private JButton boton;          // boton con una determinada accion
+	private JLabel texto1, texto2;           // etiqueta o texto no editable
+    private JButton boton1, boton2;          // boton con una determinada accion
     public String respuesta;
     private Thread thread;
 
@@ -32,7 +37,8 @@ public class Ventana1 extends JFrame implements ActionListener {
 
     private void configurarVentana() {
         this.setTitle("Space invaders");                        // colocamos titulo a la ventana
-        this.setSize(400, 300);                                 // colocamos tamanio a la ventana (ancho, alto)
+        this.getContentPane().setBackground(Color.orange);
+        this.setSize(370, 280);                                 // colocamos tamanio a la ventana (ancho, alto)
         this.setLocationRelativeTo(null);                       // centramos la ventana en la pantalla
         this.setLayout(null);                                   // no usamos ningun layout, solo asi podremos dar posiciones a los componentes
         this.setResizable(false);                               // hacemos que la ventana no sea redimiensionable
@@ -43,39 +49,45 @@ public class Ventana1 extends JFrame implements ActionListener {
         // creamos los componentes
         texto1 = new JLabel();
         texto2 = new JLabel();
-        caja1 = new JTextField();
-        caja2 = new JTextField();
-        boton = new JButton();
+        boton1 = new JButton();
+        boton2 = new JButton();
         // configuramos los componentes
-        texto1.setText("    CARGANDO NIVEL 1... "); 
-        texto2.setText("¿Desea cargar historial\n de jugadores? si/no ");   // colocamos un texto a la etiqueta
-        texto1.setBounds(30, 5, 500, 100);   // colocamos posicion y tamaño al texto (x, y, ancho, alto)
-        texto2.setBounds(30, 30, 400, 100);
-        caja2.setBounds(180, 100, 50, 30);    // colocamos posicion y tamaño a la caja2 (x, y, ancho, alto)
-        respuesta = caja2.getText();
-        
-        boton.setText("Continuar");              // colocamos un texto al boton
-        boton.setBounds(80, 200, 220, 40);     // colocamos posicion y tamaño al boton (x, y, ancho, alto)
-        boton.addActionListener(this);         // hacemos que el boton tenga una accion y esa accion estara en esta clase
+        texto1.setText(" CARGANDO NIVEL 1... "); 
+        texto2.setText("     ¿Desea cargar historial de jugadores?");   // colocamos un texto a la etiqueta
+        texto1.setBounds(125, 5, 500, 100);   // colocamos posicion y tamaÃ±o al texto (x, y, ancho, alto)
+        texto2.setBounds(60, 30, 400, 100);
        
-        // añadimos los componentes a la ventana
+        
+        boton1.setText("si");
+        boton1.setBackground(Color.green);
+        boton1.setBounds(70,100,80,40);  		 // colocamos posicion y tamaÃ±o al boton (x, y, ancho, alto)
+        boton1.addActionListener(this);
+        boton2.setText("no");
+        boton2.setBackground(Color.red);
+        boton2.setBounds(220,100,80,40);
+        boton2.addActionListener(this);
+       
+        // aniadimos los componentes a la ventana
         
         this.add(texto2);
-        this.add(caja2);
         this.add(texto1);
-        this.add(caja1);
-        this.add(boton);
+        this.add(boton1);
+        this.add(boton2);
     }
 
 	@Override
     public void actionPerformed(ActionEvent e) {
-         
-        respuesta = caja2.getText();// obtenemos el contenido de la caja de texto
-        if(respuesta.equals("si"))
-        	JOptionPane.showMessageDialog(this, "El historial está vacio. "+"\n\n La partida comenzará enseguida");
-        if(respuesta.equals("no"))
-        	JOptionPane.showMessageDialog(this, "La partida comenzará enseguida");
-        thread.resume();
+		
+		 if(e.getActionCommand().equals("si")){//podemos comparar por el contenido del boton
+	            JOptionPane.showMessageDialog(this, "El historial esta vacio. "+"\n\n La partida comenzara enseguida!");
+	            respuesta = boton1.getText();
+	        }
+
+		 if(e.getActionCommand().equals("no")){//podemos comparar por el contenido del boton
+	            JOptionPane.showMessageDialog(this, "Iniciando la partida!");
+	            respuesta = boton2.getText();
+	        }
+           thread.resume();
     }
 
     public void open() {
@@ -83,4 +95,5 @@ public class Ventana1 extends JFrame implements ActionListener {
         setVisible(true);
         thread.suspend();
     }
+        
 }
